03c8b25208eb44da01c5bc5c6d67a0849cb15f5e
has16008@byui.edu <has16008@byui.edu>
2021-02-01 22:05:00 -0700

    add images 020121 10:04pm

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -8,3 +8,12 @@ function toggleMenu(){
  document.getElementById("date").innerHTML = day[current.getDay()] +
      ", " + current.getDate() + " " + month[current.getMonth()] + " " +
      current.getFullYear();
+
+    // getDay() days are from 0-6 //
+    //var d = getDay();//
+    // if d = 6{//
+//document.getElementById("satBanner").innerHTML = d;//
+// }
+//else {
+d = "hide";
+//}//
