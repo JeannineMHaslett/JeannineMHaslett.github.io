889045ba4ef04bbfaea99d6c513e2ac5a284771c
has16008@byui.edu <has16008@byui.edu>
2021-02-02 15:42:47 -0700

    ...

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -1,19 +1,21 @@
-function toggleMenu(){
+// function for hamburger menu//
+function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
- }
- var current = new Date();
- var day = [" Sunday", " Monday", "Tuesday", " Wednesday", " Thursday", "Friday", " Saturday"];
- var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
- 
- document.getElementById("date").innerHTML = day[current.getDay()] +
-     ", " + current.getDate() + " " + month[current.getMonth()] + " " +
-     current.getFullYear();
+}
 
-    // getDay() days are from 0-6 //
-    //var d = getDay();//
-    // if d = 6{//
-//document.getElementById("satBanner").innerHTML = d;//
-// }
-//else {
-d = "hide";
-//}//
+//function for date on footer//
+var current = new Date();
+var day = [" Sunday", " Monday", "Tuesday", " Wednesday", " Thursday", "Friday", " Saturday"];
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+document.getElementById("date").innerHTML = day[current.getDay()] +
+    ", " + current.getDate() + " " + month[current.getMonth()] + " " +
+    current.getFullYear();
+
+//function for Saturday pancake banner//
+/*var d = new Date();
+var n = d.getDay();
+console.log(n);
+if (n == 2) {
+    document.getElementById("satBanner").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+}*/
