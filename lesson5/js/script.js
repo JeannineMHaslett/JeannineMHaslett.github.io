9c5b2200d262147662ec92c0d40806e0102e28e8
has16008@byui.edu <has16008@byui.edu>
2021-02-06 04:05:49 -0700

    020621 04:05am

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -1,9 +1,9 @@
-// function for hamburger menu//
+//function for hamburger menu
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
 
-//function for date on footer//
+//function for date on footer
 var current = new Date();
 var day = [" Sunday", " Monday", "Tuesday", " Wednesday", " Thursday", "Friday", " Saturday"];
 var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -12,9 +12,13 @@ document.getElementById("date").innerHTML = day[current.getDay()] +
     ", " + current.getDate() + " " + month[current.getMonth()] + " " +
     current.getFullYear();
 
-//function for Saturday pancake banner//
+//function for Saturday pancake banner
 let d = new Date();
 let n = d.getDay();
 // console.log(n);
-if (n == 5) {
-        document.querySelector(".satBanner").style.display = "block";}
+if (n == 6) {
+    document.querySelector(".satBanner").style.display = "block";
+}
+
+
+  
