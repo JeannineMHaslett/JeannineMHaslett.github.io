f9b329927559ec71661e42c16321e3db09a7d9af
has16008@byui.edu <has16008@byui.edu>
2021-02-10 12:45:40 -0700

    Lesson 5 fixes

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -4,18 +4,17 @@ function toggleMenu() {
 }
 
 //function for date on footer
-var current = new Date();
-var day = [" Sunday", " Monday", "Tuesday", " Wednesday", " Thursday", "Friday", " Saturday"];
-var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let current = new Date();
+let day = [" Sunday", " Monday", "Tuesday", " Wednesday", " Thursday", "Friday", " Saturday"];
+let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 document.getElementById("date").innerHTML = day[current.getDay()] +
     ", " + current.getDate() + " " + month[current.getMonth()] + " " +
     current.getFullYear();
 
 //function for Saturday pancake banner
-let d = new Date();
-let n = d.getDay();
-// console.log(n);
+let n = Date.getDay();
+
 if (n == 5) {
     document.querySelector(".satBanner").style.display = "block";
 }
