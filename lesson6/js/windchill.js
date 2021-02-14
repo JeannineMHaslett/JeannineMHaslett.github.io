16654c0517a52e4f3d76319e67a0f1c9c2dd171d
has16008@byui.edu <has16008@byui.edu>
2021-02-13 18:51:47 -0700

    ...

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,19 +1,16 @@
+var x = document.getElementById("temp").value; 
+var y = document.getElementById("speed").value; 
 
-let x = document.getElementById("temp").value;
-let y = document.getElementById("speed").value;
+t = parseInt(document.getElementById("temp").innerHTML); 
+s = parseInt(document.getElementById("speed").innerHTML); 
+if (t <= 50 && s >= 3) { 
+document.getElementById('windchill').innerHTML = windchill(t,s) + "&#176;"; 
+} 
+else { 
+document.getElementById("windchill").text = "N/A"; 
+} 
+function windchill(t,s) { 
+let w = Math.round(35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16)); return w; 
+} 
 
-temp = 29;
-speed = 3;
- 
-if (temp <= 50 && speed >= 3)  {
-  document.getElementById('windchill').innerHTML = windchill(t,s) + "&#176;";;
-}
-else {
-  document.getElementById("windchill").text = "N/A";
-}
-
-function windchill(t,s) {
-  let w = Math.round(35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16));
-return w;
-}
 
