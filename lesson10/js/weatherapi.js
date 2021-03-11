46dd91a985de76684650a82cda92b3e8ebb34118
has16008@byui.edu <has16008@byui.edu>
2021-03-11 16:12:27 -0700

    ...

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,17 +1,18 @@
-const requestURL="'https://api.openweathermap.org/...";
+const apiURL ='https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=940c61dc043548cf22846b340486879f&units=imperial';
 
-const apiURL = "...";
 fetch(apiURL)
 .then((response) => response.json())
 .then((jsObject) => {
-    console.log(jsObject);
+    //console.log(jsObject);
+
+    const temperature = document.querySelector('#temperature');
+    temperature.textContent = jsObject.main.temp;
+    
+    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;   
+    const desc =jsObject.weather[0].description;                                           
+    document.getElementById('imagesrc').textcontent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);                         
+    document.getElementById('icon').setAttribute('alt', desc);
+    
 });
 
-document.getElementById('current-temp').textContent = jsObject.main.temp;
-const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  //concatenate icon and png
-const desc = jsObject.weather[0].description;  //referenceweather array
-document.getElementById('imagesrc').textContent = imagesrc;  
-document.getElementById('icon').setAttribute('src', imagesrc);  
-document.getElementById('icon').setAttribute('alt', desc);
-
-<span img id = "icon" src="" alt = "describe icon"></span>
