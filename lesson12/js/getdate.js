c46fc7ac20b05269a4720d1029cce081a5961bfb
has16008@byui.edu <has16008@byui.edu>
2021-03-29 20:29:19 -0600

    ...

---
+++ lesson12/js/getdate.js
@@ -0,0 +1,7 @@
+let current = new Date();
+let day = [" Sunday", " Monday", "Tuesday", " Wednesday", " Thursday", "Friday", " Saturday"];
+let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+document.getElementById("date").innerHTML = day[current.getDay()] +
+    ", " + current.getDate() + " " + month[current.getMonth()] + " " +
+    current.getFullYear();
