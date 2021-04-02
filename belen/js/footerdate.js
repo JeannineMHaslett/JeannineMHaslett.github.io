94322b3ba24f44bcbb1d22a925d0a8f422c41634
has16008@byui.edu <has16008@byui.edu>
2021-04-01 18:03:55 -0600

    attributes done

--- belen/js/footerdate.js
+++ belen/js/footerdate.js
@@ -1,9 +1,9 @@
-    let today = new Date();
-    let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    
-    document.getElementById("today").innerHTML = weekday[today.getDay()] +
-        ", " + today.getDate() + " " + months[today.getMonth()] + " " +
-        today.getFullYear();
+let today = new Date(document.lastModified);
+let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+document.getElementById("today").innerHTML = weekday[today.getDay()] +
+    ", " + today.getDate() + " " + months[today.getMonth()] + " " +
+    today.getFullYear();
 
      
