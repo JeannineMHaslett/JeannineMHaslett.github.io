6cf81d8bda05118767e44225d500e1e066183b6c
has16008@byui.edu <has16008@byui.edu>
2021-04-05 23:39:52 -0600

    ...

--- belen/js/business.js
+++ belen/js/business.js
@@ -5,23 +5,24 @@ fetch(requestURL)
         return response.json();
     })
     .then(function (jsonObject) {
+            console.log(jsonObject);
             const business = jsonObject['business'];
             const cards = document.querySelector('.cards');
 
-            prophets.forEach(prophet => {
+            business.forEach(each_business => {
                 let card = document.createElement('section');
                 let h2 = document.createElement('h2');
                 let p = document.createElement('p');
                 let p2 = document.createElement('p');
+                let p3 = document.createElement('p');
                 let pimg = document.createElement('img');
 
-                //use template literals
-                h2.textContent = `${business.name}`;
-                pimg.setAttribute("src", business.imageurl);
-                pimg.setAttribute('alt', `Photo of ${business.name}`)
-                p.textContent = `Address: ${business.address}`;
-                p2.textContent = `Phone: ${business.phone}`;
-                p3.textContent = `Website: ${business.website}`;
+                h2.textContent = `${each_business.name}`;
+                pimg.setAttribute("src", `${each_business.Imageurl}`);
+                pimg.setAttribute('alt', `Photo of ${each_business.name}`)
+                p.textContent = `Address: ${each_business.address}`;
+                p2.textContent = `Phone: ${each_business.phone}`;
+                p3.textContent = `website: ${each_business.website}`;
 
                 pimg.style.width = '250px';
 
@@ -33,3 +34,5 @@ fetch(requestURL)
                 cards.append(card);
             })
         });
+
+        
--- belen/js/toggle.js
+++ belen/js/toggle.js
@@ -1,4 +1,3 @@
-//function for hamburger menu
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
